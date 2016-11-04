8cc22ff1419bd411fef732e8e762c7648b1b3f65
chenhao666666 <357597800@qq.com>
2016-11-04 13:44:24 +0800

    Add files via upload

---
+++ PhpProject1/opencart/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8889/admin/');
+define('HTTP_CATALOG', 'http://localhost:8889/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8889/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8889/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/web/opencart/admin/');
+define('DIR_SYSTEM', 'D:/web/opencart/system/');
+define('DIR_IMAGE', 'D:/web/opencart/image/');
+define('DIR_LANGUAGE', 'D:/web/opencart/admin/language/');
+define('DIR_TEMPLATE', 'D:/web/opencart/admin/view/template/');
+define('DIR_CONFIG', 'D:/web/opencart/system/config/');
+define('DIR_CACHE', 'D:/web/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/web/opencart/system/storage/download/');
+define('DIR_LOGS', 'D:/web/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/web/opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/web/opencart/system/storage/upload/');
+define('DIR_CATALOG', 'D:/web/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '112.124.13.197');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'dlhchh$#!35866');
+define('DB_DATABASE', 'mycnopencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
